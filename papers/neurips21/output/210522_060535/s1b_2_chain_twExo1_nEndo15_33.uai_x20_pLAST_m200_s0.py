90b23f3b949a8f6952de655da6f69ccbb3b9364d
Rafael Cabañas <rafacabanas@gmail.com>
2021-05-22 08:50:35 +0000

    results update

---
+++ papers/neurips21/output/210522_060535/s1b_2_chain_twExo1_nEndo15_33.uai_x20_pLAST_m200_s0.py
@@ -0,0 +1,1 @@
+results=dict(seed=0, markovian=False, pnsExact_u=2.192614280108785E-6, error=True, nEndo=15, errorMsg='java.util.concurrent.TimeoutException', timeExact=1104, compatible=False, file='chain_twExo1_nEndo15_33.uai', folder='s1b_2', timeBuild=749512, exoTW=1, groundtruth='cve', nExo=8, pnsExact_l=5.815229929145785E-7)

4c2c8b09ec0ffa191850621900a1d962c1ab70d6
Rafael Cabañas <rafacabanas@gmail.com>
2021-05-24 10:39:29 +0000

    update results

---
+++ papers/neurips21/output/210523_131019/s2_chain_twExo2_nEndo15_0.uai_x30_pLAST_m200_s0.py
@@ -0,0 +1,1 @@
+results=dict(file='chain_twExo2_nEndo15_0.uai', folder='s2', seed=0, markovian=False, exoTW=2, groundtruth='saturation', error=True, nExo=13, nEndo=15, errorMsg='java.util.concurrent.TimeoutException')

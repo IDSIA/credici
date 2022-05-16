6257e16a5412f8864f2d80cac5d02e0368197a69
Rafael Cabañas <rcabanas@ual.es>
2022-05-16 16:53:41 +0200

    updated script

--- papers/pgm22/code/run.sh
+++ papers/pgm22/code/run.sh
@@ -2,4 +2,4 @@
 #
 
 nohup ./selectbiasexp.py synthetic/1000/set1 0 5 > output0.out 2>&1 &
-nohup ./selectbiasexp.py synthetic/1000/set1 5 11 > output0.out 2>&1 &
+nohup ./selectbiasexp.py synthetic/1000/set1 5 11 > output1.out 2>&1 &

db39583825df6826a15eb2235db1c1c588e5b7db
Rafael Cabañas <rcabanas@ual.es>
2022-05-22 09:14:43 +0200

    fixed run script

--- papers/pgm22/code/run.sh
+++ papers/pgm22/code/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-nohup ./selectbiasexp.py synthetic/1000/set1 0 2 > output0.out 2>&1 &
-nohup ./selectbiasexp.py synthetic/1000/set1 2 6 > output1.out 2>&1 &
-nohup ./selectbiasexp.py synthetic/1000/set1 6 8 > output3.out 2>&1 &
-nohup ./selectbiasexp.py synthetic/1000/set1 8 11 > output4.out 2>&1 &
+nohup python -u ./selectbiasexp.py synthetic/1000/set2 0 2 > output0.out 2>&1 &
+nohup python -u ./selectbiasexp.py synthetic/1000/set2 2 6 > output1.out 2>&1 &
+nohup python -u ./selectbiasexp.py synthetic/1000/set2 6 8 > output2.out 2>&1 &
+nohup python -u ./selectbiasexp.py synthetic/1000/set2 8 11 > output3.out 2>&1 &

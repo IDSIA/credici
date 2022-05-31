ef95359322757fbbd6c4c13db6b5335ca7f3680d
Rafael Cabañas <rcabanas@ual.es>
2022-05-31 11:57:44 +0200

    timeout in syntheticgen

--- papers/pgm22/code/syntheticgen.py
+++ papers/pgm22/code/syntheticgen.py
@@ -71,7 +71,7 @@ def strtime():
 
 #### Function that interacts with credici
 
-def generate(topology, nEndo, markovian=True, datasize=1000, maxdist=2, reduction=1.0, query=True, timeout=120, seed = None):
+def generate(topology, nEndo, markovian=True, datasize=1000, maxdist=2, reduction=1.0, query=True, timeout=120, seed = None, min_timeout = None):
     args = ""
     args += f"-o {output_folder} "
     args += f"-n {nEndo} "
@@ -91,6 +91,8 @@ def generate(topology, nEndo, markovian=True, datasize=1000, maxdist=2, reductio
 
     print(args)
     cmd = f"{java} -Xmx{28*1024}m -cp {jar_file} {javafile} {args}"
+    if min_timeout is not None:
+        cmd = f"timeout {timeout}m {cmd}"
     print(cmd)
     exec_bash_print(cmd)
 
@@ -116,12 +118,15 @@ for seed in SEEDS:
     for nEndo in [4,5,6]:
         for reduction in [0.5, 0.75, 1.0]:
             for markovian in [False, True]:
-                args = dict(topology=topology, nEndo=nEndo, markovian=markovian, reduction=reduction, seed=seed, datasize=datasize)
-                print(f"{i}: args = {args}")
+                args = dict(topology=topology, nEndo=nEndo, markovian=markovian, reduction=reduction, seed=seed, datasize=datasize, min_timeout=5)
+                #print(f"{i}: args = {args}")
                 i += 1
                 if overwrite or not generated(args):
+                    print(f"{i}: args = {args}")
                     generate(**args)
-                    print("model generated")
+                    #print("model generated")
+                else: print(f"{i}: args = {args} -- generated")
+
 print("finished")
 
 

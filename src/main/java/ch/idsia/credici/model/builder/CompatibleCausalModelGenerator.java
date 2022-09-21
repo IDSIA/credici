647e5c282ae76bd0b57d23e594aaa195c42d5e67
Rafael Cabañas <rcabanas@ual.es>
2022-09-21 09:22:34 +0200

    set desired markovianity SCM sampling

--- src/main/java/ch/idsia/credici/model/builder/CompatibleCausalModelGenerator.java
+++ src/main/java/ch/idsia/credici/model/builder/CompatibleCausalModelGenerator.java
@@ -5,7 +5,11 @@ import ch.idsia.credici.model.StructuralCausalModel;
 import ch.idsia.credici.utility.DAGUtil;
 import ch.idsia.credici.utility.experiments.Logger;
 import ch.idsia.crema.model.graphical.SparseDirectedAcyclicGraph;
+import ch.idsia.crema.utility.RandomUtil;
 import gnu.trove.map.TIntIntMap;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class CompatibleCausalModelGenerator {
 
@@ -14,6 +18,8 @@ public class CompatibleCausalModelGenerator {
 
     /** Number of nodes in the first DAG .*/
     private int numNodes = 5;
+
+    private int markovianity = -1;  // -1 not-fixed, 0 markovian, 1 quasi-markovian, 2 non-markovian
 
     /** Parameter in case of DAGfromBNG=false*/
     private int lambda = 3;
@@ -72,6 +78,11 @@ public class CompatibleCausalModelGenerator {
         return this;
     }
 
+    public CompatibleCausalModelGenerator setMarkovianity(int markovianity) {
+        this.markovianity = markovianity;
+        return this;
+    }
+
     // Generation loop
     public void run(){
         boolean compatibleFound = false;
@@ -102,19 +113,40 @@ public class CompatibleCausalModelGenerator {
 
         // todo: maybe we could give the option to specify the markovianity here or not
         SparseDirectedAcyclicGraph dag = null;
+        boolean repeat;
+        int markObtained;
 
-        if(DAGfromBNG){
-            dag = DAGUtil.randomFromBNGenerator(); // note: you might use some of the parameters in the alternative generator
-        }else{
-            dag = DAGUtil.random(numNodes, lambda, maxIndegree);
-        }
+        do {
+            if (DAGfromBNG) {
+                dag = DAGUtil.randomFromBNGenerator(); // note: you might use some of the parameters in the alternative generator
+            } else {
+                dag = DAGUtil.random(numNodes, lambda, maxIndegree);
+            }
 
-        logger.debug("DAG before making valid: "+dag);
 
-        // step 2,3
-        dag = CausalGraphTools.makeValidCausalDAG(dag);  // todo: implement the code inside
+            // step 2,3
+            if (!CausalGraphTools.isValidCausalDAG(dag)) {
+                logger.debug("Non-valid sampled DAG before transformation: " + dag);
+                dag = CausalGraphTools.makeValidCausalDAG(dag);
+            }
+            markObtained = CausalGraphTools.getMarkovianity(dag);
 
-        logger.info("Sampled DAG: "+dag);
+            if(markovianity >=0 && markovianity != markObtained) {
+                logger.debug("Sampled DAG: " + dag);
+                logger.debug("Markovianity obtained is "+markObtained+"while desired is "+markovianity+". Resampling DAG.");
+                repeat = true;
+            }else{
+                repeat = false;
+            }
+
+
+        }while(repeat);
+
+        logger.info("Sampled DAG: " + dag);
+        logger.info("Markovianity: "+markObtained);
+
+
+
         return dag;
     }
 
@@ -123,6 +155,9 @@ public class CompatibleCausalModelGenerator {
     private void sampleCausalModel(SparseDirectedAcyclicGraph causalDAG) {
         //todo: set this.model
         logger.info("Creating SCM ");
+        logger.info("Exo CC: "+ CausalGraphTools.exoConnectComponents(causalDAG).stream().map(c -> Arrays.toString(c)).collect(Collectors.joining("|")));
+        logger.info("Endo CC: "+CausalGraphTools.endoConnectComponents(causalDAG).stream().map(c -> Arrays.toString(c)).collect(Collectors.joining("|")));
+
     }
 
 
@@ -141,18 +176,22 @@ public class CompatibleCausalModelGenerator {
     }
 
     public static void main(String[] args) {
+        int s = 2;
 
-        // Set the logging
-        Logger.setGlobal(new Logger().setLabel("model_gen").setToStdOutput(true));
+            RandomUtil.setRandomSeed(s);
 
-        CompatibleCausalModelGenerator gen = new CompatibleCausalModelGenerator()
-                                            .setNumNodes(5);
+            // Set the logging
+            Logger.setGlobal(new Logger().setLabel("model_gen").setToStdOutput(true).setLevel(Logger.Level.DEBUG));
 
-        gen.run();
+            CompatibleCausalModelGenerator gen = new CompatibleCausalModelGenerator()
+                    .setMaxIndegree(1)
+                    //.setLambda(2)
+                    .setMarkovianity(0)
+                    .setNumNodes(20);
 
-        StructuralCausalModel m = gen.getModel();
-        TIntIntMap data = gen.getData();
-
+            gen.run();
+            StructuralCausalModel m = gen.getModel();
+            TIntIntMap data = gen.getData();
 
     }
 
--- src/main/java/ch/idsia/credici/model/tools/CausalGraphTools.java
+++ src/main/java/ch/idsia/credici/model/tools/CausalGraphTools.java
@@ -2,12 +2,15 @@ package ch.idsia.credici.model.tools;
 
 import ch.idsia.credici.utility.DAGUtil;
 import ch.idsia.crema.model.graphical.SparseDirectedAcyclicGraph;
-import org.apache.commons.lang3.NotImplementedException;
+import ch.idsia.crema.utility.ArraysUtil;
 import org.jgrapht.Graph;
 import org.jgrapht.alg.clique.ChordalGraphMaxCliqueFinder;
 import org.jgrapht.graph.DirectedAcyclicGraph;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CausalGraphTools {
     public static SparseDirectedAcyclicGraph getExogenousDag(SparseDirectedAcyclicGraph dag){
@@ -32,40 +35,105 @@ public class CausalGraphTools {
     }
 
     public static SparseDirectedAcyclicGraph makeValidCausalDAG(SparseDirectedAcyclicGraph dag) {
-        //todo implement
-        return dag;
+        SparseDirectedAcyclicGraph newDag = dag.copy();
+
+        int[] internal = DAGUtil.getNonRootNodes(dag);
+        int[] roots = DAGUtil.getRootNodes(dag);
+
+        // Get the problematic nodes
+        int[] toFixNodes = IntStream.of(internal)
+                .filter(x -> ArraysUtil.intersection(dag.getParents(x), roots).length == 0)
+                .toArray();
+
+        int nextID = Arrays.stream(dag.getVariables()).max().getAsInt() + 1;
+        for(int x : toFixNodes) {
+            newDag.addVariable(nextID);
+            newDag.addLink(nextID, x);
+            nextID++;
+        }
+
+        return newDag;
     }
 
-    public static SparseDirectedAcyclicGraph isValidCausalDAG(SparseDirectedAcyclicGraph dag) {
-        throw new NotImplementedException("Not implemented");
+    public static boolean isValidCausalDAG(SparseDirectedAcyclicGraph dag) {
+        //All non-root nodes have at least a parent that is root
+        int[] internal = DAGUtil.getNonRootNodes(dag);
+        int[] roots = DAGUtil.getRootNodes(dag);
+        return IntStream.of(internal)
+                .allMatch(x -> ArraysUtil.intersection(dag.getParents(x), roots).length > 0);
+
     }
 
-    public int[] getExogenous(){
-        throw new NotImplementedException("Not implemented");
+    public static int[] getExogenous(SparseDirectedAcyclicGraph dag){
+        return DAGUtil.getRootNodes(dag);
     }
 
-    public int[] getEndogenous(){
-        throw new NotImplementedException("Not implemented");
+    public static int[] getEndogenous(SparseDirectedAcyclicGraph dag){
+        return DAGUtil.getNonRootNodes(dag);
     }
 
-   public boolean isExogenous(){
-       throw new NotImplementedException("Not implemented");
+
+   public static boolean isExogenous(SparseDirectedAcyclicGraph dag, int v){
+       return dag.getParents(v).length==0;
    }
-   public boolean isEndogenous(){
-       throw new NotImplementedException("Not implemented");
+   // We assume that the DAG is valid for SCMs
+   public static boolean isEndogenous(SparseDirectedAcyclicGraph dag, int v){
+       return !isExogenous(dag, v);
    }
 
-   public boolean isMarkovian(SparseDirectedAcyclicGraph dag){
-       throw new NotImplementedException("Not implemented");
+    public static int[] getEndogenousParents(SparseDirectedAcyclicGraph dag, int v){
+        return IntStream.of(dag.getParents(v)).filter( x -> isEndogenous(dag, x)).toArray();
+    }
+    public static int[] getEndogenousChildren(SparseDirectedAcyclicGraph dag, int v){
+        return IntStream.of(dag.getChildren(v)).filter( x -> isEndogenous(dag, x)).toArray();
+    }
+    public static int[] getExogenousParents(SparseDirectedAcyclicGraph dag, int v){
+        return IntStream.of(dag.getParents(v)).filter( x -> isExogenous(dag, x)).toArray();
+    }
+    public static int[] getExogenousChildren(SparseDirectedAcyclicGraph dag, int v){
+        return IntStream.of(dag.getChildren(v)).filter( x -> isExogenous(dag, x)).toArray();
+    }
+
+
+   public static boolean isMarkovian(SparseDirectedAcyclicGraph dag){
+        return IntStream.of(getExogenous(dag)).allMatch(u -> getEndogenousChildren(dag, u).length==1);
    }
 
-   public boolean isQuasiMarkovian(SparseDirectedAcyclicGraph dag){
-       throw new NotImplementedException("Not implemented");
+   public static boolean isQuasiMarkovian(SparseDirectedAcyclicGraph dag){
+       return IntStream.of(getEndogenous(dag)).allMatch(x -> getExogenousParents(dag, x).length==1);
    }
 
-   public boolean isNonQuasiMarkovian(SparseDirectedAcyclicGraph dag){
-       throw new NotImplementedException("Not implemented");
+   public static boolean isNonQuasiMarkovian(SparseDirectedAcyclicGraph dag){
+       return IntStream.of(getEndogenous(dag)).anyMatch(x -> getExogenousParents(dag, x).length>1);
    }
+
+   public static int getMarkovianity(SparseDirectedAcyclicGraph dag){
+        if (isMarkovian(dag)) return 0;
+        if (isQuasiMarkovian(dag)) return 1;
+        return 2;
+   }
+
+   public static List<int[]> exoConnectComponents(SparseDirectedAcyclicGraph dag){
+        return DAGUtil.connectComponents(getExogenousDag(dag))
+                .stream()
+                .map(c -> IntStream.of(c).filter(v -> isExogenous(dag, v)).toArray())
+                .collect(Collectors.toList());
+   }
+
+    public static List<int[]> endoConnectComponents(SparseDirectedAcyclicGraph dag){
+        return DAGUtil.connectComponents(getExogenousDag(dag))
+                .stream()
+                .map(c -> IntStream.of(c).filter(v -> isEndogenous(dag, v)).toArray())
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        SparseDirectedAcyclicGraph dag = DAGUtil.build("(0,1),(1,2)");
+        System.out.println(dag);
+        System.out.println(isValidCausalDAG(dag));
+        System.out.println(makeValidCausalDAG(dag));
+
+    }
 
 
 

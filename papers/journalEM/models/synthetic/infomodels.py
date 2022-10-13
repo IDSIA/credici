2c3dd975e3b8d853d064c9682222cbc91ab98584
Rafael Cabañas <rcabanas@ual.es>
2022-10-13 16:11:19 +0200

    script minor changes

---
+++ papers/journalEM/models/synthetic/infomodels.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+import subprocess
+import datetime
+import os
+import sys
+
+
+from datetime import datetime
+from pathlib import Path
+import pandas as pd
+
+#### Parameter experiments
+
+print(sys.argv)
+folder = sys.argv[1]
+

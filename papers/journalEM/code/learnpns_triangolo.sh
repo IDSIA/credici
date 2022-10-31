6c6a585e0cb9f09a7ea4cafd5b5ab858017bd02e
Rafael Cabañas <rcabanas@ual.es>
2022-10-31 15:23:11 +0100

    200 runs for triangolo

--- papers/journalEM/code/learnpns_triangolo.sh
+++ papers/journalEM/code/learnpns_triangolo.sh
@@ -4,3 +4,7 @@ sbatch --exclusive --export=init_seed=0 -n 25 learnpns_triangolo.sbs
 sbatch --exclusive --export=init_seed=25 -n 25 learnpns_triangolo.sbs
 sbatch --exclusive --export=init_seed=50 -n 25 learnpns_triangolo.sbs
 sbatch --exclusive --export=init_seed=75 -n 25 learnpns_triangolo.sbs
+sbatch --exclusive --export=init_seed=100 -n 25 learnpns_triangolo.sbs
+sbatch --exclusive --export=init_seed=125 -n 25 learnpns_triangolo.sbs
+sbatch --exclusive --export=init_seed=150 -n 25 learnpns_triangolo.sbs
+sbatch --exclusive --export=init_seed=175 -n 25 learnpns_triangolo.sbs

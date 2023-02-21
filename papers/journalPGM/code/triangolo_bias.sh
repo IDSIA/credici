9774b1d3fc68183e0f69b42d81d9683b6a970b69
Rafael Cabañas <rcabanas@ual.es>
2023-02-21 19:11:16 +0100

    scripts triangolo

--- papers/journalPGM/code/triangolo_bias.sh
+++ papers/journalPGM/code/triangolo_bias.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 ## usage: sbatch --export=init_seed=0 -n 50 triangolobiaslearn.sbs
-
-sbatch  --export=init_seed=0 -n 25 triangolobiaslearn.sbs
-sbatch  --export=init_seed=25 -n 25 triangolobiaslearn.sbs
-sbatch  --export=init_seed=50 -n 25 triangolobiaslearn.sbs
-sbatch  --export=init_seed=75 -n 25 triangolobiaslearn.sbs
+#
+#sbatch  --export=init_seed=0 -n 25 triangolobiaslearn.sbs
+#sbatch  --export=init_seed=25 -n 25 triangolobiaslearn.sbs
+#sbatch  --export=init_seed=50 -n 25 triangolobiaslearn.sbs
+#sbatch  --export=init_seed=75 -n 25 triangolobiaslearn.sbs
+sbatch  --export=init_seed=100 -n 25 triangolobiaslearn.sbs
+sbatch  --export=init_seed=125 -n 25 triangolobiaslearn.sbs
+sbatch  --export=init_seed=150 -n 25 triangolobiaslearn.sbs
+sbatch  --export=init_seed=175 -n 25 triangolobiaslearn.sbs

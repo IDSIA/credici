5c72073b14b2e9c57fd1c16a9249a2922a171f45
Rafael Cabañas <rcabanas@ual.es>
2023-02-21 13:57:10 +0100

    additional combinations

--- papers/journalPGM/code/learn_multi_study2.sh
+++ papers/journalPGM/code/learn_multi_study2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sbatch  --export=init_id=0,seed=0 -n 10 learn_multi_study.sbs
-sbatch  --export=init_id=10,seed=0 -n 10 learn_multi_study.sbs
-sbatch  --export=init_id=20,seed=0 -n 10 learn_multi_study.sbs
-sbatch  --export=init_id=30,seed=0 -n 10 learn_multi_study.sbs
-sbatch  --export=init_id=40,seed=0 -n 10 learn_multi_study.sbs
-sbatch  --export=init_id=50,seed=0 -n 10 learn_multi_study.sbs
-sbatch  --export=init_id=60,seed=0 -n 10 learn_multi_study.sbs
-sbatch  --export=init_id=70,seed=0 -n 10 learn_multi_study.sbs
-sbatch  --export=init_id=80,seed=0 -n 10 learn_multi_study.sbs
-sbatch  --export=init_id=90,seed=0 -n 10 learn_multi_study.sbs
+sbatch  --export=init_id=0,seed=0 -n 10 learn_multi_study2.sbs
+sbatch  --export=init_id=10,seed=0 -n 10 learn_multi_study2.sbs
+sbatch  --export=init_id=20,seed=0 -n 10 learn_multi_study2.sbs
+sbatch  --export=init_id=30,seed=0 -n 10 learn_multi_study2.sbs
+sbatch  --export=init_id=40,seed=0 -n 10 learn_multi_study2.sbs
+sbatch  --export=init_id=50,seed=0 -n 10 learn_multi_study2.sbs
+sbatch  --export=init_id=60,seed=0 -n 10 learn_multi_study2.sbs
+sbatch  --export=init_id=70,seed=0 -n 10 learn_multi_study2.sbs
+sbatch  --export=init_id=80,seed=0 -n 10 learn_multi_study2.sbs
+sbatch  --export=init_id=90,seed=0 -n 10 learn_multi_study2.sbs
 
 

34eefc370aa28297366cf7bf4587e1bad39e7c2e
Rafael Cabañas <rcabanas@ual.es>
2023-06-12 13:20:19 +0200

    asia params

--- papers/journalEM/code/asiapns.sh
+++ papers/journalEM/code/asiapns.sh
@@ -2,17 +2,17 @@
 
 
 # asia, tub, smoke, bronc, lung
-sbatch  --export=cause="asia",method="CCALP" -n 1 asiapns.sbs
-sbatch  --export=cause="tub",method="CCALP" -n 1 asiapns.sbs
-sbatch  --export=cause="smoke",method="CCALP" -n 1 asiapns.sbs
-sbatch  --export=cause="bronc",method="CCALP" -n 1 asiapns.sbs
-sbatch  --export=cause="lung",method="CCALP" -n 1 asiapns.sbs
+#sbatch  --export=cause="asia",method="CCALP" -n 1 asiapns.sbs
+#sbatch  --export=cause="tub",method="CCALP" -n 1 asiapns.sbs
+#sbatch  --export=cause="smoke",method="CCALP" -n 1 asiapns.sbs
+#sbatch  --export=cause="bronc",method="CCALP" -n 1 asiapns.sbs
+#sbatch  --export=cause="lung",method="CCALP" -n 1 asiapns.sbs
 
-sbatch  --export=cause="asia",method="CCVE" -n 1 asiapns.sbs
-sbatch  --export=cause="tub",method="CCVE" -n 1 asiapns.sbs
+#sbatch  --export=cause="asia",method="CCVE" -n 1 asiapns.sbs
+#sbatch  --export=cause="tub",method="CCVE" -n 1 asiapns.sbs
 sbatch  --export=cause="smoke",method="CCVE" -n 1 asiapns.sbs
-sbatch  --export=cause="bronc",method="CCVE" -n 1 asiapns.sbs
-sbatch  --export=cause="lung",method="CCVE" -n 1 asiapns.sbs
+#sbatch  --export=cause="bronc",method="CCVE" -n 1 asiapns.sbs
+#sbatch  --export=cause="lung",method="CCVE" -n 1 asiapns.sbs
 
 sbatch  --export=cause="asia",method="EMCC" -n 1 asiapns.sbs
 sbatch  --export=cause="tub",method="EMCC" -n 1 asiapns.sbs

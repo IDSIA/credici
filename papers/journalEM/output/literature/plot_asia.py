705146ee44410013a0d662b085d9546eed8209d7
Rafael Cabañas <rcabanas@ual.es>
2023-06-15 17:42:25 +0200

    plot asia

---
+++ papers/journalEM/output/literature/plot_asia.py
@@ -0,0 +1,84 @@
+
+
+import pandas as pd
+import os
+from pathlib import Path
+
+
+wdir = Path("/Users/rcabanas/GoogleDrive/IDSIA/causality/dev/credici/")
+
+
+res_folder = Path(wdir, "./papers/journalEM/output/literature/")
+
+
+
+files = [Path(res_folder, f) for f in os.listdir(res_folder) if f.endswith(".csv")]
+
+
+data = pd.concat([pd.read_csv(f) for f in files])
+
+
+cause = "tub"
+method = "EMCC"
+r = 200
+
+def get_interval(cause,method,r=200):
+    cols = [f"pns_{i}" for i in range(0,r if method=="EMCC" else 2)]
+    pns_values = data.loc[(data.cause==cause) & (data.method==method)][cols]
+    if len(pns_values)>0:
+        return round(pns_values.min(axis=1)[0],3), round(pns_values.max(axis=1)[0],3)
+    return None,None
+
+
+methods = data.method.unique()
+causes = data.cause.unique()
+
+
+varnames = dict(bronc="Bronchitis", lung="Lung Cancer", tub="Tuberculosis", smoke="Smoker", asia="Asia")
+causes = ["bronc", "lung", "tub", "smoke", "asia"]
+methods = ["CCVE", "CCALP", "EMCC"]
+
+
+tablestr = ""
+for c in causes:
+    tablestr += f"{varnames[c]}\t\t"
+    for m in methods:
+        l,u = get_interval(c,m)
+        tablestr +="\t\t& "
+        if None not in (l,u):
+            tablestr += f"$[{l},{u}]$"
+        else:
+            tablestr += " - "
+    tablestr += "\\\\ \n"
+
+print(tablestr)
+
+df = data[["cause", "method", "pns_l", "pns_u"]].sort_values(by="cause")
+
+df.loc[df.method=="CCALP"][["cause","pns_l", "pns_u"]].sort_values(by=["pns_l","pns_u"], ascending=False)
+
+
+data.columns
+
+data[[f"llk_{i}" for i in range(200)]].max().max()
+
+data[[f"llk_{i}" for i in range(200)]].min().min()
+
+
+
+
+
+
+
+
+data
+
+
+
+'''
+Bronchitis   & $[xx,xx]$ & $[xx,xx]$ & $[xx,xx]$ &  \\
+Lung Cancer  & -         & $[xx,xx]$ & $[xx,xx]$ &  \\
+Tuberculosis & $[xx,xx]$ & $[xx,xx]$ & $[xx,xx]$ &  \\
+Smoker       & -         & $[xx,xx]$ & $[xx,xx]$ &  \\
+Asia         & $[xx,xx]$ & $[xx,xx]$ & $[xx,xx]$ & 
+'''

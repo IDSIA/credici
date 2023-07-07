c77567573d3947d6307880109fd501dec5eb59e1
Rafael Cabañas <rcabanas@ual.es>
2023-07-07 17:17:31 +0200

    ASIA journalEM rerun

--- papers/journalEM/code/asiapns.sh
+++ papers/journalEM/code/asiapns.sh
@@ -14,9 +14,9 @@ sbatch  --export=cause="smoke",method="CCVE" -n 1 asiapns.sbs
 sbatch  --export=cause="bronc",method="CCVE" -n 1 asiapns.sbs
 sbatch  --export=cause="lung",method="CCVE" -n 1 asiapns.sbs
 
-sbatch  --export=cause="asia",method="EMCC" -n 1 asiapns.sbs
-sbatch  --export=cause="tub",method="EMCC" -n 1 asiapns.sbs
-sbatch  --export=cause="smoke",method="EMCC" -n 1 asiapns.sbs
-sbatch  --export=cause="bronc",method="EMCC" -n 1 asiapns.sbs
-sbatch  --export=cause="lung",method="EMCC" -n 1 asiapns.sbs
+#sbatch  --export=cause="asia",method="EMCC" -n 1 asiapns.sbs
+#sbatch  --export=cause="tub",method="EMCC" -n 1 asiapns.sbs
+#sbatch  --export=cause="smoke",method="EMCC" -n 1 asiapns.sbs
+#sbatch  --export=cause="bronc",method="EMCC" -n 1 asiapns.sbs
+#sbatch  --export=cause="lung",method="EMCC" -n 1 asiapns.sbs
 

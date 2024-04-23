54a7e01bb126d0228fb876d7c85108ae20b4010d
Rafael Cabañas <rcabanas@ual.es>
2024-04-23 16:00:12 +0200

    scripts

---
+++ papers/zeros/code/genn.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sbatch  --export=np=2,nzr=1.0,zdr=0.5,init_seed=0 -n 50 genn.sbs
+sbatch  --export=np=2,nzr=0.8,zdr=0.5,init_seed=0 -n 50 genn.sbs
+sbatch  --export=np=2,nzr=0.6,zdr=0.5,init_seed=0 -n 50 genn.sbs
+sbatch  --export=np=2,nzr=0.4,zdr=0.5,init_seed=0 -n 50 genn.sbs
+sbatch  --export=np=2,nzr=0.2,zdr=0.5,init_seed=0 -n 50 genn.sbs
+
+sbatch  --export=np=2,nzr=1.0,zdr=1.0,init_seed=0 -n 50 genn.sbs
+sbatch  --export=np=2,nzr=0.8,zdr=1.0,init_seed=0 -n 50 genn.sbs
+sbatch  --export=np=2,nzr=0.6,zdr=1.0,init_seed=0 -n 50 genn.sbs
+sbatch  --export=np=2,nzr=0.4,zdr=1.0,init_seed=0 -n 50 genn.sbs
+sbatch  --export=np=2,nzr=0.2,zdr=1.0,init_seed=0 -n 50 genn.sbs
+
+sbatch  --export=np=3,nzr=1.0,zdr=0.5,init_seed=0 -n 50 genn.sbs
+sbatch  --export=np=3,nzr=0.8,zdr=0.5,init_seed=0 -n 50 genn.sbs
+sbatch  --export=np=3,nzr=0.6,zdr=0.5,init_seed=0 -n 50 genn.sbs
+sbatch  --export=np=3,nzr=0.4,zdr=0.5,init_seed=0 -n 50 genn.sbs
+sbatch  --export=np=3,nzr=0.2,zdr=0.5,init_seed=0 -n 50 genn.sbs
+
+sbatch  --export=np=3,nzr=1.0,zdr=1.0,init_seed=0 -n 50 genn.sbs
+sbatch  --export=np=3,nzr=0.8,zdr=1.0,init_seed=0 -n 50 genn.sbs
+sbatch  --export=np=3,nzr=0.6,zdr=1.0,init_seed=0 -n 50 genn.sbs
+sbatch  --export=np=3,nzr=0.4,zdr=1.0,init_seed=0 -n 50 genn.sbs
+sbatch  --export=np=3,nzr=0.2,zdr=1.0,init_seed=0 -n 50 genn.sbs
--- papers/zeros/code/learn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sbatch  --export=maxiter=1000,init_seed=0 -n 50 learn.sbs
-sbatch  --export=maxiter=1000,init_seed=50 -n 50 learn.sbs
-sbatch  --export=maxiter=1000,init_seed=100 -n 50 learn.sbs
-sbatch  --export=maxiter=1000,init_seed=150 -n 50 learn.sbs

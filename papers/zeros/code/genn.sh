b1473d38cc1b4026e49209a14e56c0c1ffab0f02
Rafael Cabañas <rcabanas@ual.es>
2024-05-02 20:11:40 +0200

    preparing s2 with 4 nodes

--- papers/zeros/code/genn.sh
+++ papers/zeros/code/genn.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
-sbatch  --export=np=2,nzr=1.0,zdr=0.5,init_seed=0 -n 50 genn.sbs
-sbatch  --export=np=2,nzr=0.8,zdr=0.5,init_seed=0 -n 50 genn.sbs
-sbatch  --export=np=2,nzr=0.6,zdr=0.5,init_seed=0 -n 50 genn.sbs
-sbatch  --export=np=2,nzr=0.4,zdr=0.5,init_seed=0 -n 50 genn.sbs
-sbatch  --export=np=2,nzr=0.2,zdr=0.5,init_seed=0 -n 50 genn.sbs
+#sbatch  --export=np=2,nzr=1.0,zdr=0.5,init_seed=0 -n 50 genn.sbs
+#sbatch  --export=np=2,nzr=0.8,zdr=0.5,init_seed=0 -n 50 genn.sbs
+#sbatch  --export=np=2,nzr=0.6,zdr=0.5,init_seed=0 -n 50 genn.sbs
+#sbatch  --export=np=2,nzr=0.4,zdr=0.5,init_seed=0 -n 50 genn.sbs
+#sbatch  --export=np=2,nzr=0.2,zdr=0.5,init_seed=0 -n 50 genn.sbs
 
-sbatch  --export=np=2,nzr=1.0,zdr=1.0,init_seed=0 -n 50 genn.sbs
-sbatch  --export=np=2,nzr=0.8,zdr=1.0,init_seed=0 -n 50 genn.sbs
-sbatch  --export=np=2,nzr=0.6,zdr=1.0,init_seed=0 -n 50 genn.sbs
-sbatch  --export=np=2,nzr=0.4,zdr=1.0,init_seed=0 -n 50 genn.sbs
-sbatch  --export=np=2,nzr=0.2,zdr=1.0,init_seed=0 -n 50 genn.sbs
+#sbatch  --export=np=2,nzr=1.0,zdr=1.0,init_seed=0 -n 50 genn.sbs
+#sbatch  --export=np=2,nzr=0.8,zdr=1.0,init_seed=0 -n 50 genn.sbs
+#sbatch  --export=np=2,nzr=0.6,zdr=1.0,init_seed=0 -n 50 genn.sbs
+#sbatch  --export=np=2,nzr=0.4,zdr=1.0,init_seed=0 -n 50 genn.sbs
+#sbatch  --export=np=2,nzr=0.2,zdr=1.0,init_seed=0 -n 50 genn.sbs
 
-sbatch  --export=np=3,nzr=1.0,zdr=0.5,init_seed=0 -n 100 genn.sbs
-sbatch  --export=np=3,nzr=0.8,zdr=0.5,init_seed=0 -n 100 genn.sbs
-sbatch  --export=np=3,nzr=0.6,zdr=0.5,init_seed=0 -n 100 genn.sbs
-sbatch  --export=np=3,nzr=0.4,zdr=0.5,init_seed=0 -n 100 genn.sbs
-sbatch  --export=np=3,nzr=0.2,zdr=0.5,init_seed=0 -n 100 genn.sbs
+#sbatch  --export=np=3,nzr=1.0,zdr=0.5,init_seed=0 -n 100 genn.sbs
+#sbatch  --export=np=3,nzr=0.8,zdr=0.5,init_seed=0 -n 100 genn.sbs
+#sbatch  --export=np=3,nzr=0.6,zdr=0.5,init_seed=0 -n 100 genn.sbs
+#sbatch  --export=np=3,nzr=0.4,zdr=0.5,init_seed=0 -n 100 genn.sbs
+#sbatch  --export=np=3,nzr=0.2,zdr=0.5,init_seed=0 -n 100 genn.sbs
 
-sbatch  --export=np=3,nzr=1.0,zdr=1.0,init_seed=0 -n 100 genn.sbs
-sbatch  --export=np=3,nzr=0.8,zdr=1.0,init_seed=0 -n 100 genn.sbs
-sbatch  --export=np=3,nzr=0.6,zdr=1.0,init_seed=0 -n 100 genn.sbs
-sbatch  --export=np=3,nzr=0.4,zdr=1.0,init_seed=0 -n 100 genn.sbs
-sbatch  --export=np=3,nzr=0.2,zdr=1.0,init_seed=0 -n 100 genn.sbs
+#sbatch  --export=np=3,nzr=1.0,zdr=1.0,init_seed=0 -n 100 genn.sbs
+#sbatch  --export=np=3,nzr=0.8,zdr=1.0,init_seed=0 -n 100 genn.sbs
+##sbatch  --export=np=3,nzr=0.6,zdr=1.0,init_seed=0 -n 100 genn.sbs
+#sbatch  --export=np=3,nzr=0.4,zdr=1.0,init_seed=0 -n 100 genn.sbs
+#sbatch  --export=np=3,nzr=0.2,zdr=1.0,init_seed=0 -n 100 genn.sbs
+
+
+
+sbatch  --export=np=4,nzr=1.0,zdr=0.5,init_seed=0 -n 100 genn.sbs
+sbatch  --export=np=4,nzr=0.8,zdr=0.5,init_seed=0 -n 100 genn.sbs
+sbatch  --export=np=4,nzr=0.6,zdr=0.5,init_seed=0 -n 100 genn.sbs
+sbatch  --export=np=4,nzr=0.4,zdr=0.5,init_seed=0 -n 100 genn.sbs
+sbatch  --export=np=4,nzr=0.2,zdr=0.5,init_seed=0 -n 100 genn.sbs
+
+sbatch  --export=np=4,nzr=1.0,zdr=1.0,init_seed=0 -n 100 genn.sbs
+sbatch  --export=np=4,nzr=0.8,zdr=1.0,init_seed=0 -n 100 genn.sbs
+sbatch  --export=np=4,nzr=0.6,zdr=1.0,init_seed=0 -n 100 genn.sbs
+sbatch  --export=np=4,nzr=0.4,zdr=1.0,init_seed=0 -n 100 genn.sbs
+sbatch  --export=np=4,nzr=0.2,zdr=1.0,init_seed=0 -n 100 genn.sbs

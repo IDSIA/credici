ddfdd990b8e967c32d8845e004bfc55647ff39cd
Rafael Cabañas <rcabanas@ual.es>
2024-05-14 17:39:08 +0200

    cflavour4q preparing

--- papers/causal_flavour/code/learn.sh
+++ papers/causal_flavour/code/learn.sh
@@ -4,3 +4,15 @@ sbatch  --export=maxiter=1000,init_seed=0 -n 50 learn.sbs
 sbatch  --export=maxiter=1000,init_seed=50 -n 50 learn.sbs
 sbatch  --export=maxiter=1000,init_seed=100 -n 50 learn.sbs
 sbatch  --export=maxiter=1000,init_seed=150 -n 50 learn.sbs
+
+
+sbatch  --export=maxiter=500,init_seed=0 -n 50 learn.sbs
+sbatch  --export=maxiter=500,init_seed=50 -n 50 learn.sbs
+sbatch  --export=maxiter=500,init_seed=100 -n 50 learn.sbs
+sbatch  --export=maxiter=500,init_seed=150 -n 50 learn.sbs
+
+
+sbatch  --export=maxiter=1500,init_seed=0 -n 50 learn.sbs
+sbatch  --export=maxiter=1500,init_seed=50 -n 50 learn.sbs
+sbatch  --export=maxiter=1500,init_seed=100 -n 50 learn.sbs
+sbatch  --export=maxiter=1500,init_seed=150 -n 50 learn.sbs
